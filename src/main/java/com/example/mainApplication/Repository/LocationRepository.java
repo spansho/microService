2f1b1e744044a378e8bda5c03861d5328bed40fe
Egor <egordedikovrvdkr@mail.ru>
2025-03-20 21:45:46 +0300

    переделка

--- src/main/java/com/example/mainApplication/Repository/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mainApplication.Repository;
-
-import com.example.mainApplication.dto.Location;
-import com.example.mainApplication.dto.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface LocationRepository extends CrudRepository<Location,Integer> {
-}
--- src/main/java/com/example/mainApplication/Repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mainApplication.Repository;
-
-import com.example.mainApplication.dto.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PersonRepository extends CrudRepository<Person,Integer> {
-}
